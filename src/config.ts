bae50ad826534f04cd0c66ee40465743cf0762ad
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-12-16 20:35:09 +0530

    configure prod staging and dev

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,13 @@
 export const config = {
-  telegramApi: process.env.NODE_ENV === "production" ? "https://" : "http://localhost:3005",
-  backendApi: process.env.NODE_ENV === "production" ? "https://" : "http://localhost:3000",
+  telegramApi:
+    process.env.ENV === "prod"
+      ? "https://api.valuesdao.io"
+      : process.env.ENV === "staging"
+      ? "https://staging-api.valuesdao.io"
+      : "http://localhost:3005",
+  backendApi: process.env.ENV === "prod" 
+      ? "https://api.valuesdao.io" 
+      : process.env.ENV === "staging" 
+      ? "https://staging-api.valuesdao.io" 
+      : "http://localhost:3000",
 };
