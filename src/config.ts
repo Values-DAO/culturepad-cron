a29c3e943d0c905f314bc7089c0dd3ff40e5c329
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-12-17 01:11:02 +0530

    update telegramApi

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,15 @@
 export const config = {
-  telegramApi:
+  // telegramApi:
+    // process.env.ENV === "prod"
+    //   ? "https://api.valuesdao.io"
+    //   : process.env.ENV === "staging"
+    //   ? "https://staging-api.valuesdao.io"
+    //   : "http://localhost:3005",
+  telegramApi: "http://localhost:3005",
+  backendApi:
     process.env.ENV === "prod"
       ? "https://api.valuesdao.io"
       : process.env.ENV === "staging"
       ? "https://staging-api.valuesdao.io"
-      : "http://localhost:3005",
-  backendApi: process.env.ENV === "prod" 
-      ? "https://api.valuesdao.io" 
-      : process.env.ENV === "staging" 
-      ? "https://staging-api.valuesdao.io" 
       : "http://localhost:3000",
 };
